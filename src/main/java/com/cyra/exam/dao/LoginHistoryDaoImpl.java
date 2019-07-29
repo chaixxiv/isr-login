2703365a274faf5628a853964a37e7fc6b42b331
Cyra Abigail Betco <cyra.betco@gmail.com>
2019-07-30 00:22:24 +0800

    refactored loginHistoryDaoImpl

--- src/main/java/com/cyra/exam/dao/LoginHistoryDaoImpl.java
+++ src/main/java/com/cyra/exam/dao/LoginHistoryDaoImpl.java
@@ -1,100 +1,80 @@
 package com.cyra.exam.dao;
 
 import com.cyra.exam.domain.UserCount;
-import com.cyra.exam.mapper.LoginRowMapper;
 import com.cyra.exam.mapper.UserCountMapper;
-import org.apache.commons.collections4.MultiValuedMap;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.MultiValueMap;
 
-import java.text.MessageFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 public class LoginHistoryDaoImpl implements LoginHistoryDao {
+
     public static final String START_DATE = "start";
     public static final String END_DATE = "end";
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    public static final DateTimeFormatter DATE_FORMAT_YYYYMMDD = DateTimeFormatter.ofPattern("yyyyMMdd");
 
     @Override
     public List<LocalDate> getUniqueDates() {
-        return jdbcTemplate.queryForList("SELECT DISTINCT DATE(login_time) from login order by login_time asc", LocalDate.class);
+        return jdbcTemplate.queryForList("SELECT DISTINCT DATE(login_time) from login order by login_time asc", Collections.emptyMap(), LocalDate.class);
     }
 
     @Override
     public List<String> getUsersByLoginDate(Map<String, LocalDate> dateMap) {
-        StringBuilder query = new StringBuilder();
-        query.append("select distinct user from login ");
-        if (!dateMap.isEmpty()) {
+        StringBuilder query = new StringBuilder("select distinct user from login");
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        List<String> whereCriteria = new ArrayList<>();
+        if (dateMap.containsKey(START_DATE)) {
+            whereCriteria.add(" date(login_time) >= :" + START_DATE);
+            params.addValue(START_DATE, dateMap.get(START_DATE));
+        }
+        if (dateMap.containsKey(END_DATE)) {
+            whereCriteria.add(" date(login_time) <= :" + END_DATE);
+            params.addValue(END_DATE, dateMap.get(END_DATE));
+        }
+        if (!whereCriteria.isEmpty()) {
             query.append(" where ");
-            buildLoginDateRangeQuery(dateMap, query);
+            query.append(whereCriteria.stream().collect(Collectors.joining(" and ")));
         }
         query.append(" order by user asc");
-
-        return jdbcTemplate.queryForList(query.toString(), String.class);
+        return jdbcTemplate.queryForList(query.toString(), params, String.class);
     }
 
     @Override
     public List<UserCount> getUserAndLoggedTimes(MultiValueMap<String, String> queryMap) {
-        StringBuilder query = new StringBuilder();
-        query.append("select distinct user, count(user) from login ");
-        if (!queryMap.isEmpty()) {
+        StringBuilder query = new StringBuilder("select distinct user, count(user) from login");
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        List<String> whereCriteria = new ArrayList<>();
+        for (String key : queryMap.keySet()) {
+            if (key.equals(START_DATE)) {
+                whereCriteria.add(" date(login_time) >= :" + START_DATE);
+                params.addValue(START_DATE, LocalDate.parse(queryMap.getFirst(START_DATE), DATE_FORMAT_YYYYMMDD));
+            } else if (key.equals(END_DATE)) {
+                whereCriteria.add(" date(login_time) <= :" + END_DATE);
+                params.addValue(END_DATE, LocalDate.parse(queryMap.getFirst(END_DATE), DATE_FORMAT_YYYYMMDD));
+            } else {
+                whereCriteria.add(key + " in (:" + key + ")");
+                params.addValue(key, queryMap.get(key));
+            }
+        }
+        if (!whereCriteria.isEmpty()) {
             query.append(" where ");
+            query.append(whereCriteria.stream().collect(Collectors.joining(" and ")));
         }
-
-        if (queryMap.containsKey(START_DATE) || queryMap.containsKey(END_DATE)) {
-            Map<String, LocalDate> dateMap = new HashMap<>();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-            dateMap.computeIfAbsent("start", val -> queryMap.get(START_DATE) != null ? LocalDate.parse(queryMap.get(START_DATE).get(0), formatter) : null);
-            dateMap.computeIfAbsent("end", val -> queryMap.get(END_DATE) != null ? LocalDate.parse(queryMap.get(END_DATE).get(0), formatter) : null);
-            buildLoginDateRangeQuery(dateMap, query);
-            query.append(" and ");
-        }
-
-        Iterator<Map.Entry<String, List<String>>> it = queryMap.entrySet().iterator();
-        while(it.hasNext()) {
-            Map.Entry<String, List<String>> pair = it.next();
-            if (!START_DATE.equals(pair.getKey()) && !END_DATE.equals(pair.getKey())) {
-                query.append(MessageFormat.format(" {0} in ", pair.getKey()));
-                buildQueryParamsArray(queryMap.get(pair.getKey()), query);
-                if (it.hasNext()) {
-                    query.append(" and ");
-                }
-            }
-        }
-
         query.append(" group by user");
-        return jdbcTemplate.query(query.toString(), new UserCountMapper());
-    }
-
-    private void buildQueryParamsArray(List<String> queryParams, StringBuilder query) {
-        query.append("( ");
-        for (int i=0; i<queryParams.size(); i++) {
-            query.append(MessageFormat.format("\"{0}\"", queryParams.get(i)));
-            if (i != queryParams.size() - 1) {
-                query.append(",");
-            }
-        }
-        query.append(" )");
-    }
-
-    private void buildLoginDateRangeQuery(Map<String, LocalDate> dateMap, StringBuilder query) {
-        if (dateMap.containsKey(START_DATE) && dateMap.containsKey(END_DATE)) {
-            LocalDate startDate = dateMap.get(START_DATE);
-            LocalDate endDate = dateMap.get(END_DATE);
-            query.append(MessageFormat.format(" date(login_time) between \"{0}\" and \"{1}\"", startDate, endDate));
-        } else if (dateMap.containsKey(START_DATE)) {
-            LocalDate startDate = dateMap.get(START_DATE);
-            query.append(MessageFormat.format(" date(login_time) >= \"{0}\"", startDate));
-        } else if (dateMap.containsKey(END_DATE)) {
-            LocalDate endDate = dateMap.get(END_DATE);
-            query.append(MessageFormat.format(" date(login_time) <= \"{0}\"", endDate));
-        }
+        return jdbcTemplate.query(query.toString(), params, new UserCountMapper());
     }
 }
